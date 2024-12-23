17c5dd18f17fafb2d8f87d47b42a6b635079a41f
hugo <hugo@baudchon.com>
2024-12-23 17:22:12 -0500

    cleanup of launchers folder

--- launchers/object-detection.sh
+++ launchers/object-detection.sh
@@ -11,13 +11,8 @@ pip3 freeze
 dt-launchfile-init
 
 ## Set up CUDA environment
-#export CUDA_HOME=/usr/local/cuda-10.2
-#export PATH=/usr/local/cuda-10.2/bin:${PATH}
-#export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64:/usr/lib/aarch64-linux-gnu:${LD_LIBRARY_PATH}
-#export CPATH=/usr/local/cuda-10.2/targets/aarch64-linux/include:/usr/local/cuda-10.2/include:${CPATH}
 
 # launch subscriber
-#dt-exec "${DT_REPO_PATH}"/packages/pycuda/build.sh
 rosrun object_detection object_detection_node.py
 
 # wait for app to end
